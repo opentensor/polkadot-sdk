d36408a07d1981a6cc0aa520edd1d9e725295c0d
Branislav Kontur <bkontur@gmail.com>
2025-05-01 13:34:46 +0200

    Re-implement `DDayHook` with `pallet_bridge_proof_root_sync`

--- cumulus/parachains/runtimes/assets/asset-hub-westend/src/lib.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-westend/src/lib.rs
@@ -783,7 +783,7 @@ parameter_types! {
 impl cumulus_pallet_parachain_system::Config for Runtime {
 	type WeightInfo = weights::cumulus_pallet_parachain_system::WeightInfo<Runtime>;
 	type RuntimeEvent = RuntimeEvent;
-	type OnSystemEvent = (DDayHook,);
+	type OnSystemEvent = (dday_hook::DDayHeaderSyncForCollectives,);
 	type SelfParaId = parachain_info::Pallet<Runtime>;
 	type DmpQueue = frame_support::traits::EnqueueWithOrigin<MessageQueue, RelayOrigin>;
 	type ReservedDmpWeight = ReservedDmpWeight;
@@ -1125,8 +1125,6 @@ impl pallet_migrations::Config for Runtime {
 	type WeightInfo = weights::pallet_migrations::WeightInfo<Runtime>;
 }
 
-impl dday_hook::Config for Runtime {}
-
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime
@@ -1185,7 +1183,7 @@ construct_runtime!(
 		AssetRewards: pallet_asset_rewards = 61,
 
 		StateTrieMigration: pallet_state_trie_migration = 70,
-		DDayHook: dday_hook = 71,
+		DDayHook: pallet_bridge_proof_root_sync::<Instance1> = 71,
 
 		// TODO: the pallet instance should be removed once all pools have migrated
 		// to the new account IDs.
